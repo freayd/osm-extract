a6285e8ee7134123ac6187b8deb61e16357a96fe
freayd <freayd@users.noreply.github.com>
2016-05-29 19:24:59 +1000

    Add script for Australia

---
+++ australia.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+REGION=australia
+GFB_CONTINENT=australia-oceania
+GFB_REGION=australia
+OSA_REGION=australia-oceania/australia-oceania.poly
+
+source "$( dirname "${BASH_SOURCE[0]}" )/extractor.sh"
